2a044a5fda72faa5513af677f4a1a0f4d5e7f4b2
alecdalelio <alecdalelio@gmail.com>
2020-12-31 14:51:35 -0500

    changed p to li

--- index.js
+++ index.js
@@ -3,28 +3,23 @@ let toDoContainer = document.querySelector("#toDoContainer");
 let inputField = document.querySelector("#inputField");
 
 addToDoButton.addEventListener("click", (ev) => {
-  // creates a new p element held in "paragraph" variable
-  let paragraph = document.createElement("p");
+  // creates a new li element held in "toDoItem" variable
+  let toDoItem = document.createElement("li");
 
-  // adds "paragraph-styling" class to the newly created paragraph
-  paragraph.classList.add("paragraph-styling");
+  // adds "toDo-styling" class to the newly created list item
+  toDoItem.classList.add("toDo-styling");
 
-  // applies the value of the <input> element to the newly created paragraph
-  paragraph.innerText = inputField.value;
+  // applies the value of the <input> element to the newly created list item
+  toDoItem.innerText = inputField.value;
 
-  // appends (or adds) the filled in paragraph element to the toDoContainer
-  toDoContainer.appendChild(paragraph);
+  // appends (or adds) the filled in list item element to the toDoContainer
+  toDoContainer.appendChild(toDoItem);
 
   // resets the value of the input field to empty so you can type more to-dos
   inputField.value = "";
 
-  // creates an event listener so that when the paragraph is clicked, its style (text-decoration) changes so that it appears striked through
-  paragraph.addEventListener("click", function () {
-    paragraph.style.textDecoration = "line-through";
-  });
-
-  // creates another event listener so that when a paragraph is clicked a second time, it is removed from the page
-  paragraph.addEventListener("dblclick", function () {
-    toDoContainer.removeChild(paragraph);
+  // creates another event listener so that when a paragraph is clicked, it is removed from the page
+  toDoItem.addEventListener("click", function () {
+    toDoContainer.removeChild(toDoItem);
   });
 });
